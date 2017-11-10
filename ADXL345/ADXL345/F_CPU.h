1b9f671d607ff38ca54c2b45d3ffa2613181f0ec
geneLocated <genelocated@foxmail.com>
2017-11-10 22:32:41 +0800

    Add files I wrote

---
+++ ADXL345/ADXL345/F_CPU.h
@@ -0,0 +1,15 @@
+/*
+ * F_CPU.h
+ * 请在此处定义F_CPU
+ * Created: 2017/10/4 10:38:51
+ *  Author: geneLocated
+ */ 
+
+
+#ifndef F_CPU_H_
+#define F_CPU_H_
+
+#define F_CPU	8000000
+#define baud	9600
+
+#endif /* F_CPU_H_ */
---
+++ Clock/Clock/main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ * 时间应用库测试程序
+ * Created: 2017/10/3 16:31:55
+ * Author : geneLocated
+ */ 
+
+#include "F_CPU.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include "APP_Clock.h"
+#include "HAL_USART.h"
+
+
+int main(void)
+{
+	Clock_Init();
+	USART_Begin();
+	sei();
+    while (1) 
+    {
+		for (unsigned char i=0; i<3; i++)
+		{
+			USART_Print(time[2-i]);
+			USART_SendByte('\t');
+		}
+		USART_SendLine();
+		_delay_ms(500);
+	}
+}
+
